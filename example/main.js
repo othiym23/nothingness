09a5653611e0f62feb25ae2b4688c0aec8a381ad
Forrest L Norvell <forrest@npmjs.com>
2015-08-02 21:49:06 -0700

    improved promise chain in example thanks to @zkat

--- example/main.js
+++ example/main.js
@@ -6,16 +6,12 @@ const dao = new ThingerDAO(new Adaptor('./thinger-db'))
 const thingy = { type: 'band' }
 
 // uses Bluebird's .nodeify(), so callback or promise chain are fine
-dao
-  .save(thingy)
-  .then(() => {
-    dao
-      .findAll()
-      .then(results => assert.deepEqual(
-        results,
-        [{ type: 'band' }],
-        'should only have one item, of type "band"'
-      ))
-      .then(() => console.log('round trip succeeded!'))
-      .catch(err => console.error(err.stack))
-  })
+dao.save(thingy)
+   .then(() => dao.findAll())
+   .then(results => assert.deepEqual(
+     results,
+     [{ type: 'band' }],
+     'should only have one item, of type "band"'
+   ))
+   .then(() => console.log('round trip succeeded!'))
+   .catch(err => console.error(err.stack))
